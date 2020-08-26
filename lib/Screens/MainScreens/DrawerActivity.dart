661938cb1558f1fa2699ab5bc3fb5c80db3c3a42
shahrukhktk <Shahrukhktk760@gmail.com>
2020-08-26 00:01:41 -0700

    1. splash > done 2. login > done 3. code verification > done 4. reset pswrd > done 5. reset success >done 6. Login via Fingerprint > done 7. fingerprint success > done 8. Login via Face recognition > done 9. Face success > done 10. sign up > done 11. sign up success > done ------------------------------------ 11. upload pic > done 12. upload pic success > done 13. Choose Church > done 14. choose church success > done 15. Add Church Details > done 16. add church success > done 17. Walk into church > done ---------------------------------- 18. Main Drawer 19. Home Screen

--- lib/Screens/MainScreens/DrawerActivity.dart
+++ lib/Screens/MainScreens/DrawerActivity.dart
@@ -10,6 +10,12 @@ class _MainDrawerActivityState extends State<MainDrawerActivity> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title:  Text('Home', style: TextStyle(
+          fontFamily: 'SFProDisplay', fontSize: 18.0, fontWeight: FontWeight.bold, color: Colors.white,
+        ),),
+        backgroundColor: AppColor.themeColor,
+      ),
       drawer: Drawer(
         child: ListView(
           padding: EdgeInsets.zero,
@@ -50,6 +56,77 @@ class _MainDrawerActivityState extends State<MainDrawerActivity> {
                 color: AppColor.themeColor,
               ),
             ),
+
+            ListTile(
+              onTap: (){
+
+              },
+              leading: Image(image: AssetImage('images/channel.png'),),
+              title: Text('Channel', style: TextStyle(
+                fontFamily: 'SFProDisplay', fontSize: 16.0, fontWeight: FontWeight.w500, color: Colors.black,
+              ),),
+            ),
+
+            ListTile(
+              onTap: (){
+
+              },
+              leading: Image(image: AssetImage('images/rooms.png'),),
+              title: Text('Rooms', style: TextStyle(
+                fontFamily: 'SFProDisplay', fontSize: 16.0, fontWeight: FontWeight.w500, color: Colors.black,
+              ),),
+            ),
+
+            ListTile(
+              onTap: (){
+
+              },
+              leading: Image(image: AssetImage('images/payment.png'),),
+              title: Text('Payment', style: TextStyle(
+                fontFamily: 'SFProDisplay', fontSize: 16.0, fontWeight: FontWeight.w500, color: Colors.black,
+              ),),
+            ),
+
+            ListTile(
+              onTap: (){
+
+              },
+              leading: Image(image: AssetImage('images/schedule.png'),),
+              title: Text('Schedule', style: TextStyle(
+                fontFamily: 'SFProDisplay', fontSize: 16.0, fontWeight: FontWeight.w500, color: Colors.black,
+              ),),
+            ),
+
+            ListTile(
+              onTap: (){
+
+              },
+              leading: Image(image: AssetImage('images/notes.png'),),
+              title: Text('Notes', style: TextStyle(
+                fontFamily: 'SFProDisplay', fontSize: 16.0, fontWeight: FontWeight.w500, color: Colors.black,
+              ),),
+            ),
+
+            ListTile(
+              onTap: (){
+
+              },
+              leading: Image(image: AssetImage('images/manage_assigned.png'),),
+              title: Text('Manage Assigned', style: TextStyle(
+                fontFamily: 'SFProDisplay', fontSize: 16.0, fontWeight: FontWeight.w500, color: Colors.black,
+              ),),
+            ),
+
+            ListTile(
+              onTap: (){
+
+              },
+              leading: Image(image: AssetImage('images/rededicated.png'),),
+              title: Text('Re-dedicate', style: TextStyle(
+                fontFamily: 'SFProDisplay', fontSize: 16.0, fontWeight: FontWeight.w500, color: Colors.black,
+              ),),
+            ),
+
           ],
         ),
       ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:rccg_app/Screens/Constant/constant.dart';
 import 'Screens/splashscreen.dart';
 
 void main() => runApp(new MaterialApp(
   theme: ThemeData(
-    primaryColor: Colors.white,
-    accentColor: Colors.white,
+    primaryColor: AppColor.themeColor,
+    accentColor: AppColor.themeColor,
     fontFamily: "Monteserrat",
   ),
   debugShowCheckedModeBanner: false,
